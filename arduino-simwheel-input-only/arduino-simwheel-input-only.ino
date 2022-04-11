365021bef9d8aac395d6edc7df02cfa4eaf2478c
lasevi <las3vi@gmail.com>
2022-04-11 16:47:34 +0300

    Starting to work on FFB, WIP
    q

--- arduino-simwheel-input-only/arduino-simwheel-input-only.ino
+++ arduino-simwheel-input-only/arduino-simwheel-input-only.ino
@@ -1,23 +1,13 @@
-// Arduino Joystick with FFB library from here:
-//   https://github.com/YukMingLaw/ArduinoJoystickWithFFBLibrary
+// Arduino Joystick library from here: https://github.com/MHeironimus/ArduinoJoystickLibrary
 #include <Joystick.h>
-
-
-#define DEBUG false
 
 #define ENCODER_MAX_VALUE 2100
 #define ENCODER_MIN_VALUE -2100
 #define ENCODER_OUT_A_PIN 2
 #define ENCODER_OUT_B_PIN 3
 
-#define BTS7960_RPWM_PIN 5
-#define BTS7960_LPWM_PIN 6
-#define BTS7960_R_EN_PIN 8
-#define BTS7960_L_EN_PIN 9
-
 #define AUTO_SEND_STATE false
-
-
+#define DEBUG false
 
 Joystick_ Joystick(
   JOYSTICK_DEFAULT_REPORT_ID, // hidReportId
@@ -37,33 +27,22 @@ Joystick_ Joystick(
   true  // includeSteering
 ); 
 
-
-
 //------------------------------------------
 //Note: not every pin is usable for interrupts!
 //You need to connect the signal wires to pins which are usable for interrupts: Micro, Leonardo, other 32u4-based: 0, 1, 2, 3, 7
 volatile int32_t encoder_state = 0; // volatile is using RAM this is because we will use interrupts
 int32_t encoder_previous_state = 0;
 
-// FFB
-Gains mygains[2];
-EffectParams myeffectparams[2];
-int32_t forces[2] = {0};
 
-
-
-void encoderOutAChange() {
+void encoder_out_A_change() {
   // when outA changes, outA==outB means negative direction
   encoder_state += digitalRead(ENCODER_OUT_A_PIN) == digitalRead(ENCODER_OUT_B_PIN) ? -1 : 1; 
 }
 
-
-void encoderOutBChange() {
+void encoder_out_B_change() {
   // when outB changes, outA==outB means positive direction
   encoder_state += digitalRead(ENCODER_OUT_A_PIN) == digitalRead(ENCODER_OUT_B_PIN) ? 1 : -1;
 }
-
-
 
 void setup() {
   #if DEBUG
@@ -71,13 +50,12 @@ void setup() {
   #endif
   
   Joystick.begin(AUTO_SEND_STATE);
-
-  // ENCODER
+  
   Joystick.setSteeringRange(ENCODER_MIN_VALUE, ENCODER_MAX_VALUE);
-
-  pinMode(ENCODER_OUT_A_PIN, INPUT_PULLUP);
-  pinMode(ENCODER_OUT_B_PIN, INPUT_PULLUP);
-
+  
+  pinMode(ENCODER_OUT_A_PIN, INPUT_PULLUP); //Encoder pin
+  pinMode(ENCODER_OUT_B_PIN, INPUT_PULLUP); //Encoder pin
+  
   attachInterrupt(
     digitalPinToInterrupt(ENCODER_OUT_A_PIN),
     encoder_out_A_change,
@@ -86,20 +64,10 @@ void setup() {
     digitalPinToInterrupt(ENCODER_OUT_B_PIN),
     encoder_out_B_change,
     CHANGE);
-  // ENCODER
-
-  // BTS7960 MOTOR DRIVER
-  pinMode(BTS7960_RPWM_PIN, OUTPUT);
-  pinMode(BTS7960_LPWM_PIN, OUTPUT);
-  pinMode(BTS7960_R_EN_PIN, INPUT);
-  pinMode(BTS7960_L_EN_PIN, INPUT);
-  // BTS7960 MOTOR DRIVER
-
 }
 
-
 void loop() {
-  // ENCODER & STEERING AXIS
+  
   if (encoder_previous_state != encoder_state) {
     if(encoder_state < ENCODER_MIN_VALUE){
       encoder_state = ENCODER_MIN_VALUE;
@@ -113,26 +81,10 @@ void loop() {
       Serial.println(encoder_state);
     #endif
   }
-  // ENCODER & STEERING AXIS
   
-
   #if !AUTO_SEND_STATE
     Joystick.sendState();
   #endif
-
-  // FORCE FEEDBACK
-  // Joystick.setEffectParams(myeffectparams);
-  Joystick.getForce(forces);
-  if(forces[0] > 0){
-    // digitalWrite(6,LOW);
-    // digitalWrite(7,HIGH);
-    analogWrite(9,abs(forces[0]));
-  }else{
-    // digitalWrite(6,HIGH);
-    // digitalWrite(7,LOW);
-    analogWrite(9,abs(forces[0]));
-  }
-  // FORCE FEEDBACK
 
   delay(1);
 }
