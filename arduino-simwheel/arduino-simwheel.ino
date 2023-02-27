2d0f870f52b54df1e10d6c0c189e204d2ea1cf15
lasevi <las3vi@gmail.com>
2023-02-28 01:44:07 +0200

    Change PWM freq, test the motor with a pot

--- arduino-simwheel/arduino-simwheel.ino
+++ arduino-simwheel/arduino-simwheel.ino
@@ -11,11 +11,11 @@
 #define ENCODER_MAX_VALUE 2100
 #define ENCODER_MIN_VALUE -2100
 
-#define BTS7960_RPWM_PIN 5
-#define BTS7960_LPWM_PIN 6
-#define BTS7960_R_EN_PIN 8
-#define BTS7960_L_EN_PIN 9
+#define BTS7960_RPWM_PIN 9
+#define BTS7960_LPWM_PIN 10
 
+
+#define TEMP_POT_PIN A1
 
 
 // Both X and Y axis need to be active for FFB to work, I recall.
@@ -50,6 +50,32 @@ Gains mygains[2];
 EffectParams myeffectparams[2];
 int32_t forces[2] = {0};
 
+void setPwmFrequency(){
+  // Set-up hardware PWM on the Arduino UNO/Pro Micro on digital pins D9 and D10
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(WGM11);  // Enable PWM outputs for OC1A and OC1B on digital pins 9, 10
+  //TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10);   // Set fast PWM and prescaler of 8 on timer 1
+  //ICR1 = 999;                                     // Set the PWM frequency to 2kHz (16MHz / (8 * (999 + 1)))
+  TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10);     // Set fast PWM and prescaler of 1 on timer 1
+  ICR1 = 800;                                       // Set the PWM frequency to 20kHz (16MHz / (1 * (799 + 1)))
+  OCR1A = 0;                                        // Set duty-cycle to 0% on D9
+  OCR1B = 0.5*ICR1;                                 // Set duty-cycle to 50% on D10
+}
+
+
+void setPwmDutyCycle(int pin, int duty_cycle){
+  /*
+    I got some strange behaviour with analogWrite and ICR1 799.
+    It's like duty cycle rises too slowly first and jumps to 100% at the end.
+    I suspect analogWrite only works on when ICR1 is the default value (255?).
+   */
+  if(pin == 9){
+    OCR1A = duty_cycle;
+  }
+  if(pin == 10){
+    OCR1B = duty_cycle;
+  }
+}
+
 
 
 void encoderOutAChange() {
@@ -66,6 +92,7 @@ void encoderOutBChange() {
 
 
 void setup() {
+  setPwmFrequency();
   #if DEBUG
     Serial.begin(9600); 
   #endif
@@ -89,11 +116,6 @@ void setup() {
   // BTS7960 MOTOR DRIVER
   pinMode(BTS7960_RPWM_PIN, OUTPUT);
   pinMode(BTS7960_LPWM_PIN, OUTPUT);
-  pinMode(BTS7960_R_EN_PIN, OUTPUT);
-  pinMode(BTS7960_L_EN_PIN, OUTPUT);
-  // TODO set enablers high
-  digitalWrite(BTS7960_R_EN_PIN, HIGH);
-  digitalWrite(BTS7960_L_EN_PIN, HIGH);
   // BTS7960 MOTOR DRIVER
 
   // FFB gains
@@ -103,6 +125,8 @@ void setup() {
   myeffectparams[0].springMaxPosition = 1023;
   Joystick.setEffectParams(myeffectparams);
   Joystick.begin(AUTO_SEND_STATE);
+
+  pinMode(TEMP_POT_PIN, INPUT);
 }
 
 
@@ -117,13 +141,15 @@ void loop() {
     }
     encoder_previous_state = encoder_state;
   }
+
+
   Joystick.setXAxis(encoder_state);
   Joystick.setYAxis(0);
   // ENCODER & STEERING AXIS
   
 
   #if !AUTO_SEND_STATE
-    Joystick.sendState();
+    //Joystick.sendState();
   #endif
 
   // FORCE FEEDBACK
@@ -131,26 +157,33 @@ void loop() {
       ENCODER_MIN_VALUE, ENCODER_MAX_VALUE,
       0, 1023
     );
-  Joystick.setEffectParams(myeffectparams);
-  Joystick.getForce(forces);
-  if(forces[0] > 0){
+  //Joystick.setEffectParams(myeffectparams);
+  //Joystick.getForce(forces);
+
+  int potVal = analogRead(TEMP_POT_PIN);
+  
+  int rPWM = map(potVal, 512, 1023, 0, 799);
+  int lPWM = map(potVal, 512, 0, 0, 799);
+
+
+  if(potVal >= 512){
     // Forward force (R)
-    analogWrite(BTS7960_RPWM_PIN, abs(forces[0]));
-    analogWrite(BTS7960_LPWM_PIN, 0);
+    setPwmDutyCycle(BTS7960_RPWM_PIN, rPWM);
+    setPwmDutyCycle(BTS7960_LPWM_PIN, 0);
   }else{
     // Reverse force (L)
-    analogWrite(BTS7960_RPWM_PIN, 0);
-    analogWrite(BTS7960_LPWM_PIN, abs(forces[0]));
+    setPwmDutyCycle(BTS7960_RPWM_PIN, 0);
+    setPwmDutyCycle(BTS7960_LPWM_PIN, lPWM);
   }
   // FORCE FEEDBACK
 
   #if DEBUG
-    Serial.print("encoder: ");
-    Serial.print(encoder_state);
-    Serial.print(" forces[0]: ");
-    Serial.print(forces[0]);
-    Serial.print(" forces[1]: ");
-    Serial.print(forces[1]);
+    Serial.print("potval: ");
+    Serial.print(potVal);
+    Serial.print(" rWPM: ");
+    Serial.print(rPWM);
+    Serial.print(" lPWM: ");
+    Serial.print(lPWM);
     Serial.println();
   #endif
   delay(1);
