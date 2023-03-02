66eb1328985e9a28d8f06a2fc24a12d7c93d6a06
lasevi <las3vi@gmail.com>
2023-03-02 20:45:18 +0200

    Add wheel button code

--- arduino-simwheel/arduino-simwheel.ino
+++ arduino-simwheel/arduino-simwheel.ino
@@ -18,9 +18,17 @@
 #define THROTTLE_POTENTIOMETER_PIN A0
 #define BRAKE_POTENTIOMETER_PIN A1
 
-#define SHIFT_REG_Q_PIN 8  // Shift register serial output
+#define SHIFT_REG_DATA_PIN 8  // Shift register serial output
 #define SHIFT_REG_PL_PIN 6 // PL, parallel load input (pin9 on the HEF4021BT)
 #define SHIFT_REG_CP_PIN 4 // CP, clock input, (pin10 on the HEF4021BT)
+// Test:
+//   Parallel Load works with 400us
+//   Works with 350us
+//   Doesn't work with 300us
+//   Too low values result in corrupted values.
+#define SHIFT_REG_PL_WAIT_MICROSECONDS 400
+#define SHIFT_REGISTER_BITS 24
+
 
 
 // Both X and Y axis need to be active for FFB to work, I recall.
@@ -42,8 +50,6 @@ Joystick_ Joystick(
   false  // includeSteering
 ); 
 
-
-
 //------------------------------------------
 //Note: not every pin is usable for interrupts!
 //You need to connect the signal wires to pins which are usable for interrupts: Micro, Leonardo, other 32u4-based: 0, 1, 2, 3, 7
@@ -53,6 +59,15 @@ volatile int32_t encoder_state = 0; // volatile is using RAM this is because we 
 Gains mygains[2];
 EffectParams myeffectparams[2];
 int32_t forces[2] = {0};
+
+// Wheel shift registers
+uint8_t wheel_serial_bit_index = 0;
+#if DEBUG
+  uint16_t buttons_serial_data = 0;
+  uint16_t buttons_confirmed_to_work = 0;
+#endif
+
+uint8_t count = 0;
 
 
 
@@ -83,45 +98,54 @@ void setPwmDutyCycle(int pin, int duty_cycle){
 }
 
 
-void readWheelButtons() {
-  // Load the parallel values to the registers
-  digitalWrite(SHIFT_REG_PL_PIN,1);
-  delay(20);  // Delay to load the values
-  digitalWrite(SHIFT_REG_PL_PIN,0);
-  delay(20);  // Delay after loading and before reading - unecessary?
-
-  // Read all the 3 8bit shift registers
-  uint32_t shiftregisters = shiftIn(SHIFT_REG_Q_PIN, SHIFT_REG_CP_PIN, MSBFIRST) <<16;
-  shiftregisters |= shiftIn(SHIFT_REG_Q_PIN, SHIFT_REG_CP_PIN, MSBFIRST) <<8;
-  shiftregister3 |= shiftIn(SHIFT_REG_Q_PIN, SHIFT_REG_CP_PIN, MSBFIRST);
-
-  // Set the values to the joystick buttons
-  // The only the last 16 (15?) bits are used.
-  for(int i=0;i<16;i++){
-    uint32_t selector_bit = 1 << i;
-    Joystick.setButton(i, selector_bit);
+void asyncReadWheelButtons(){
+  /*
+  * This reads one button per function call, so it
+  *   takes SHIFT_REGISTER_BITS times to read all buttons.
+  *
+  * I made this to reduce read latency from ~14ms to <1ms,
+  *   compared to reading all values at once.
+  */
+  if(wheel_serial_bit_index == SHIFT_REGISTER_BITS){
+    wheel_serial_bit_index = 0;
+    // Close the latch, switch to serial mode
+    digitalWrite(SHIFT_REG_PL_PIN, LOW);
+    delayMicroseconds(SHIFT_REG_PL_WAIT_MICROSECONDS);
   }
-}
+  
+  // Pulse the clock and read a bit
+  digitalWrite(SHIFT_REG_CP_PIN, HIGH);
+  uint16_t current_bit = digitalRead(SHIFT_REG_DATA_PIN);
+  digitalWrite(SHIFT_REG_CP_PIN, LOW);
 
-
-
-uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
-  // Arduino Default shiftIn function, edited by lasevi
-  uint8_t value = 0;
-  uint8_t i;
-  for (i = 0; i < 8; ++i) {
-    delay(1);  // @lasevi, Needed to add these delays
-    digitalWrite(clockPin, HIGH);
-    delay(1);  // @lasevi, Needed to add these delays
-    if (bitOrder == LSBFIRST)
-      value |= digitalRead(dataPin) << i;
-    else
-      value |= digitalRead(dataPin) << (7 - i);
-    digitalWrite(clockPin, LOW);
+  if(wheel_serial_bit_index < 8){
+    // Skip the first 8 bits, they are not useful.
+    // The first bits just statically output something, I guess
+    // it's the steering wheel ID or revision or something like that.
+    wheel_serial_bit_index++;
+    return;
   }
-  return value;
-}
 
+  // And save the bits after the 8th bit
+  uint8_t button_index = wheel_serial_bit_index - 8;
+
+  // Set the buttons, they are pressed when low
+  Joystick.setButton(button_index, !current_bit);
+
+  #if DEBUG
+    buttons_serial_data |= current_bit << button_index;
+    if(!current_bit){
+      buttons_confirmed_to_work |= 1 << button_index;
+    }
+  #endif
+
+  if(wheel_serial_bit_index == SHIFT_REGISTER_BITS-1){
+    // Next loop I need to latch new values
+    // Opening the latch at this time saves a tiny amount of time #premature-optimization
+    digitalWrite(SHIFT_REG_PL_PIN, HIGH);
+  }
+  wheel_serial_bit_index++;
+}
 
 
 void encoderSaturation() {
@@ -132,6 +156,7 @@ void encoderSaturation() {
     encoder_state = ENCODER_MIN_VALUE;
   }
 }
+
 
 void encoderOutAChange() {
   // when outA changes, outA==outB means negative direction
@@ -147,18 +172,16 @@ void encoderOutBChange() {
 }
 
 
-
 void setup() {
   #if DEBUG
     Serial.begin(9600); 
   #endif
   
+
   // ENCODER
   Joystick.setXAxisRange(ENCODER_MIN_VALUE, ENCODER_MAX_VALUE);
-  
   pinMode(ENCODER_OUT_A_PIN, INPUT);
   pinMode(ENCODER_OUT_B_PIN, INPUT);
-
   attachInterrupt(
     digitalPinToInterrupt(ENCODER_OUT_A_PIN),
     encoderOutAChange,
@@ -169,10 +192,12 @@ void setup() {
     CHANGE);
   // ENCODER
 
+
   // BTS7960 MOTOR DRIVER
   pinMode(BTS7960_RPWM_PIN, OUTPUT);
   pinMode(BTS7960_LPWM_PIN, OUTPUT);
   // BTS7960 MOTOR DRIVER
+
 
   // FFB setup
   mygains[0].totalGain = 100;//0-100
@@ -182,6 +207,7 @@ void setup() {
   Joystick.setEffectParams(myeffectparams);
   // FFB setup
 
+
   // Pedals
   pinMode(THROTTLE_POTENTIOMETER_PIN, INPUT);
   pinMode(BRAKE_POTENTIOMETER_PIN, INPUT);
@@ -189,9 +215,18 @@ void setup() {
   Joystick.setBrakeRange(0, 1023);
   // Pedals
 
+
+  // BUTTONS
+  pinMode(SHIFT_REG_DATA_PIN, INPUT);
+  pinMode(SHIFT_REG_PL_PIN, OUTPUT);
+  pinMode(SHIFT_REG_CP_PIN, OUTPUT);
+  // BUTTONS
+
+
   // Joystick setup
   Joystick.begin(AUTO_SEND_STATE);
   // Joystick setup
+
 
   // MISC
   setPwmFrequency();
@@ -200,11 +235,6 @@ void setup() {
 
 
 void loop() {
-  // ENCODER & STEERING AXIS
-  Joystick.setXAxis(encoder_state);
-  // Joystick.setYAxis(0);  // Not used
-  // ENCODER & STEERING AXIS
-
   // THROTTLE & BRAKE AXIS
   uint16_t throttle = analogRead(THROTTLE_POTENTIOMETER_PIN);
   uint16_t brake = analogRead(BRAKE_POTENTIOMETER_PIN);
@@ -212,21 +242,31 @@ void loop() {
   Joystick.setBrake(brake);
   // THROTTLE & BRAKE AXIS
 
-  #if !AUTO_SEND_STATE
-    Joystick.sendState();
-  #endif
 
-  // FORCE FEEDBACK
+  // BUTTONS
+  asyncReadWheelButtons();
+  // BUTTONS
+
+
+  // STEERING AXIS
+  Joystick.setXAxis(encoder_state);
   myeffectparams[0].springPosition = map(encoder_state,
       ENCODER_MIN_VALUE, ENCODER_MAX_VALUE,
       0, ENCODER_RANGE
     );
   Joystick.setEffectParams(myeffectparams);
   Joystick.getForce(forces);
+  // STEERING AXIS
 
+
+  #if !AUTO_SEND_STATE && !DEBUG
+    Joystick.sendState();
+  #endif
+
+
+  // FORCE FEEDBACK
   // Scale the -255...255 force to the PWM value range
   int ffbPWM = map(abs(forces[0]), 0,  255, 0, ICR1);
-
   if(forces[0] > 0){
     // Forward force (R)
     analogWrite(BTS7960_RPWM_PIN, ffbPWM);
@@ -238,14 +278,25 @@ void loop() {
   }
   // FORCE FEEDBACK
 
+
   #if DEBUG
-    Serial.print("encoder: ");
+    Serial.print("encoder:");
     Serial.print(encoder_state);
-    Serial.print(" forces[0]: ");
-    Serial.print(forces[0]);
-    Serial.print(" forces[1]: ");
-    Serial.print(forces[1]);
+    Serial.print(" ffbPWM:");
+    Serial.print(ffbPWM);
+    Serial.print(" thr:");
+    Serial.print(throttle);
+    Serial.print(" brk:");
+    Serial.print(brake);
+    // Shift register debug
+    Serial.print(" buttons:");
+    Serial.print(buttons_serial_data, BIN);
+    Serial.print(" confirmed:");
+    Serial.print(buttons_confirmed_to_work, BIN);
+    if(buttons_confirmed_to_work == 0b111111111111111){
+      Serial.print(", all!");
+    }
+    // Shift register debug
     Serial.println();
   #endif
-  delay(1);
 }
